084e826c4e8827bdcc12edc0e2db4dd82ca16e11
lpancho <leonard.pancho@gmail.com>
2021-03-28 12:21:01 +0800

    Removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
-import 'dart:async';
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'constants.dart';
-import 'progressbar_painter.dart';
-import 'enums/action_mode.dart';
-import 'enums/operator_mode.dart';
-import 'enums/visibility_mode.dart';
-import 'tile.dart';
 import 'screens/main_menu.dart';
-
 
 void main() {
   runApp(MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:twentyfourtyeightplus/main.dart';
 
-import 'package:tester2048/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
