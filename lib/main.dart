73d07ad79ae3c9365efdde0994317e79e8f2059a
lpancho <leonard.pancho@gmail.com>
2021-03-28 13:33:10 +0800

    App Title should be 2048+

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '2048',
+      title: '2048+',
       home: MainMenuWidget(),
     );
   }
