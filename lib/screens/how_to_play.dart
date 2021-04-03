9a8ba82886df65437d10b220af6f9defa166c81b
SumPancho <SumPancho@github.com>
2021-04-03 18:59:19 +0800

    The greatest How to Play screen ever

--- lib/screens/how_to_play.dart
+++ lib/screens/how_to_play.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:twentyfourtyeightplus/constants.dart';
 
 class HowToPlay extends StatefulWidget {
   @override
@@ -14,9 +15,39 @@ class _HowToPlayState extends State<HowToPlay> {
       child: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Image(image: AssetImage('images/Slide1.PNG')),
-            Image(image: AssetImage('images/Slide2.PNG')),
-            Image(image: AssetImage('images/Slide3.PNG')),
+            Image(image: AssetImage('images/howtoplay.png')),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: tan,
+              ),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    style: buttonStyle,
+                    child: Container(
+                      width: 150.0,
+                      height: 60.0,
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        "B A C K",
+                        textAlign: TextAlign.center,
+                        style: mainMenuTextStyle,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
